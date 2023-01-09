cb437b0c9b8bfffe4ffb7c4587ff8ff97d86af90
mhizambareli <mariam07@yandex.ru>
2023-01-09 23:28:55 +0300

    2.19. часть 2. Сделала задание с машинками, бип-бип

---
+++ src/main/java/Lesson4/ru/pflb/Person.java
@@ -0,0 +1,16 @@
+package Lesson4.ru.pflb;
+
+/**
+ * Сделать абстрактный класс Person в ru.pflb
+ * Класс содержит поля:
+ * age  - возраст в годах
+ * name - строка с ФИО
+ * Класс содержит методы:
+ * - toString - возвращает строку с информацией о водителе
+ */
+public abstract class Person {
+    public int age;
+    public String name;
+
+    public abstract String toString();
+}
---
+++ src/main/java/Lesson4/ru/pflb/details/Engine.java
@@ -0,0 +1,30 @@
+package Lesson4.ru.pflb.details;
+
+/**
+ * Создать класс Engine в пакете ru.pflb.details
+ * содержит поля:
+ * power - значение мощности двигателя в ваттах
+ * vendor - название производителя
+ * - toString - возвращает строку с информацией о двигателе
+ * Формат вывода:
+ * <vendor> - <мощность>
+ */
+public class Engine {
+    int power;
+    String vendor;
+
+    /**
+     * Конструктор класса Engine
+     * @param vendor - наименование производителя
+     * @param power - мощность двигателя в ваттах
+     */
+    public Engine(String vendor, int power) {
+        this.vendor = vendor;
+        this.power = power;
+    }
+
+    @Override
+    public String toString() {
+        return vendor + " - " + power + " ватт";
+    }
+}
