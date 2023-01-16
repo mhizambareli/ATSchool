8dabf0da41abffed09d69a1a384e3495914e5b3d
mhizambareli <mariam07@yandex.ru>
2023-01-16 20:20:22 +0300

    2.31. задание 3 проверка делимости числа а на число b нацело

---
+++ src/main/java/lesson6/part3/Main.java
@@ -0,0 +1,31 @@
+package lesson6.part3;
+
+public class Main {
+
+    /**
+     * Напишите программу, которая получает два положительных числа через программные аргументы командной строки и выводит true, если первое из чисел нацело делится на другое.
+     * Проверку на кол-во введенных аргументов делать не надо.
+     * Попробуйте решить задание без использования if.
+     * Выведите исходные значения и результат. Например:
+     *
+     * 1)Дано: a = 4, b = 5
+     * false
+     *
+     * 2)Дано: a = 15, b = 5
+     * true
+     */
+    public static boolean isDivisible(String a, String b) {
+        System.out.println("Дано: a = " + a + ", b = " + b);
+        int a1 = Integer.parseInt(a); //парсим в int, т.к. в main() аргументы принимаются в String
+        int b1 = Integer.parseInt(b); //парсим в int, т.к. в main() аргументы принимаются в String
+        return a1 % b1 == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isDivisible(args[0], args[1]));
+    }
+
+
+}
+
+
