a0bab393f9d8586c402d27266238f80951da6f62
mhizambareli <mariam07@yandex.ru>
2023-05-28 20:41:06 +0300

    7.9.1 изменения только в классе main, создан arraylist, принимающий только фрукты, реализация с помощью stream api

---
+++ src/main/java/lesson19/part1/Plant.java
@@ -0,0 +1,5 @@
+package lesson19.part1;
+
+public interface Plant {
+    String getName();
+}
---
+++ src/main/java/lesson19/part1/vegetables/Tomato.java
@@ -0,0 +1,10 @@
+package lesson19.part1.vegetables;
+
+import lesson19.part1.vegetables.Vegetable;
+
+public class Tomato implements Vegetable {
+    @Override
+    public String getName() {
+        return "Помидор";
+    }
+}
