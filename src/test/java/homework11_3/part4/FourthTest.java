becb1dc084b309fa29341530b85e099863579afc
Mariam Khizambareli <mariam07@yandex.ru>
2023-07-19 22:58:56 +0300

    11.3 часть 4
    Тест страницы со скрытым элементом c текстом Hello World!

--- src/test/java/homework11_3/part4/FourthTest.java
+++ src/test/java/homework11_3/part4/FourthTest.java
@@ -1,12 +1,11 @@
 package homework11_3.part4;
 
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class FourthTest {
 
@@ -16,16 +15,23 @@ public class FourthTest {
      * the-internet.herokuapp.com/dynamic_loading/1,
      * в котором:
      * - отрывается страница
-     * - проверяется, что отсутсвует элемент с текстом "Hello World"
+     * - проверяется, что отсутствует элемент с текстом "Hello World"
      * - нажимается кнопка
      * - через некоторое время  появляется элемент с текстом
      */
+
     @Test
-    void hiddenGreetings(){
-        open("the-internet.herokuapp.com/dynamic_loading/1");
+    void hiddenGreetings() {
+        open("https://the-internet.herokuapp.com/dynamic_loading/1");
         getWebDriver().manage().window().maximize();
-        //assertFalse($("div#finish").isDisplayed()); todo перепиши почекай
-
+        SelenideElement helloWorldBlock = $("div#finish");
+        SelenideElement startButton = $("div#start button");
+        SelenideElement helloWorldText = $("div#finish h4");
+        helloWorldBlock.shouldHave(attribute("style", "display: none;"));
+        helloWorldText.shouldHave(exactTextCaseSensitive(""));
+        startButton.click();
+        sleep(10000);
+        helloWorldBlock.shouldHave(attribute("style", ""));
+        helloWorldText.shouldHave(exactTextCaseSensitive("Hello World!"));
     }
-
 }
